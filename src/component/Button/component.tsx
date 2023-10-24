e652be5880474a1dea4c5b461ce93f9e3f208478
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-10-24 18:59:37 +0800

    Able to expand if no mine found

--- src/component/Button/component.tsx
+++ src/component/Button/component.tsx
@@ -4,7 +4,7 @@ import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
 import ErrorIcon from '@mui/icons-material/Error';
 import { store } from "@store";
 import { useSnapshot } from "valtio";
-import { getNearbyCell } from "../../service/game";
+import { getNearByMineNumber, getNearbyCell, Location, } from "../../service/game";
 
 type Props = {
   backdropHandler: (state: boolean) => void;
@@ -24,10 +24,7 @@ export default function Button(p: Props) {
   const cell = sp.game.cell[p.row][p.column];
 
   // find nearby mine number
-  const nearByMineNumber = getNearbyCell(p, sp.board)
-    .map((item) => sp.game.cell[item[0]][item[1]].isMine)
-    .filter(Boolean)
-    .length;
+  const nearByMineNumber = getNearByMineNumber(p, store);
 
   const revealedColor = cell.flag == -1 ? "" : "hover:bg-sky-100 bg-sky-300";
 
@@ -43,13 +40,40 @@ export default function Button(p: Props) {
     }
 
     store.game.cell[p.row][p.column].flag = -1;
-    if (!cell.isMine) return;
+    if (cell.isMine) {
+      // gg if this is mine
+      store.game.gameOver = true;
+      p.backdropHandler(true);
+    }
+    if (nearByMineNumber > 0) {
+      return;
+    }
+    // TODO: traverse all nearby cell recursively if they are 0
+    const queue: Array<Location> = [{ row: p.row, column: p.column, }];
+    const visited = new Set<string>();
+    while (queue.length > 0) {
+      const current = queue.shift()!;
+      visited.add(`${current.row}-${current.column}`);
+      store.game.cell[current.row][current.column].flag = -1; // now visit this cell
 
-    store.game.gameOver = true;
-    p.backdropHandler(true);
+      getNearbyCell(current, store.board)
+        .filter((item) => sp.game.cell[item[0]][item[1]].flag != -1) // ensure this cell is not visited
+        .filter((item) => getNearByMineNumber({ row: item[0], column: item[1] }, store) == 0) // find cell that matches requirement
+        .filter((item) => !visited.has(`${item[0]}-${item[1]}`))
+        .forEach((item) => queue.push({ row: item[0], column: item[1], }));
+
+      getNearbyCell(current, store.board)
+        .filter((item) => sp.game.cell[item[0]][item[1]].flag != -1)
+        .filter((item) => getNearByMineNumber({ row: item[0], column: item[1] }, store) > 0)
+        .forEach((item) => store.game.cell[item[0]][item[1]].flag = -1);
+    }
   };
   const rightClickHandler = (e) => {
     e.preventDefault();
+    if (cell.flag == -1) {
+      // do not change if already revealed
+      return;
+    }
     store.game.cell[p.row][p.column].flag = (cell.flag + 1) % 3;
   };
 
--- src/service/game.tsx
+++ src/service/game.tsx
@@ -1,6 +1,6 @@
-import { Cell } from "@store";
+import { Cell, Store } from "@store";
 
-interface Location {
+export interface Location {
   row: number;
   column: number;
 }
@@ -22,7 +22,18 @@ export const initializeBoard = (row: number, column: number, mine: number,) => {
     }, []);
 };
 
+export const getNeighbourCell = (location: Location, board: Location) => [-1, 0, 1].flatMap((rowItem) => [-1, 0, 1].map((columnItem) => [rowItem, columnItem]))
+  .filter((item) => item[0] == 0 || item[1] == 0)
+  .map((item) => [location.row + item[0], location.column + item[1]])
+  .filter((item) => item[0] != location.row || item[1] != location.column)
+  .filter((item) => 0 <= item[0] && item[0] < board.row && 0 <= item[1] && item[1] < board.column);
+
 export const getNearbyCell = (location: Location, board: Location) => [-1, 0, 1].flatMap((rowItem) => [-1, 0, 1].map((columnItem) => [rowItem, columnItem]))
   .filter((item) => item[0] != 0 || item[1] != 0)
   .map((item) => [location.row + item[0], location.column + item[1]])
   .filter((item) => 0 <= item[0] && item[0] < board.row && 0 <= item[1] && item[1] < board.column);
+
+export const getNearByMineNumber = (location: Location, store: Store) => getNearbyCell(location, store.board)
+  .map((item) => store.game.cell[item[0]][item[1]].isMine)
+  .filter(Boolean)
+  .length;
